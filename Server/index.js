79c7853c581729ec68b7b250f71dd218681c2e78
PraDeepKuMaR <97429335+pradeepkumar24rk@users.noreply.github.com>
2023-04-15 14:24:17 +0530

    first commit

---
+++ Server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app= express();
+const http= require('http');
+const { Server, Socket }=require('socket.io')
+const cors=require('cors');
+app.use(cors);
+
+
+const server=http.createServer(app);
+
+const io=new Server(server,{
+    cors: {
+        origin:'http://localhost:3000',
+        methods:['GET','POST']
+    }
+})
+
+io.on('connection',(socket)=>{
+    console.log(socket.id);
+})
+
+
